eff731394f0f09b8437c79245d4b02999c2b2325
hakgagik <xddarkgalegh@aim.com>
2015-03-02 19:38:18 -0500

    Initialization code

--- deliverables/gameplay/core/src/edu/cornell/cs3152/gameplayprototype/Board.java
+++ deliverables/gameplay/core/src/edu/cornell/cs3152/gameplayprototype/Board.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.math.*;
 import com.badlogic.gdx.assets.*;
 import com.badlogic.gdx.graphics.*;
 
+import java.security.PublicKey;
+
 
 /**
  * Board class for storing information about board tiles.
@@ -14,13 +16,39 @@ import com.badlogic.gdx.graphics.*;
  * As well as other things :P Fill in description here.
  */
 public class Board {
+	public int width;
+	public int height;
 
 	public TileState[][] tiles;
+
+	public Board(int w, int h){
+		width = w;
+		height = h;
+		tiles = new TileState[width][height];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				tiles[i][j] = new TileState();
+			}
+		}
+	}
+
+
+	/** Clears the board, calling clear on each tile. */
+	public void clear() {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				tiles[i][j].clear();
+			}
+		}
+	}
+
 
 	public void update(float delta) {
 		// TODO: Update the board; E.g state of each tile
 		// Might not need this method.
 	}
+
+
 
 	public static class TileState {
 		public boolean goal;
@@ -28,5 +56,21 @@ public class Board {
 		public boolean knight;
 		public boolean enemy;
 		public boolean obstacle;
+
+		public TileState() {
+			goal = false;
+			start = false;
+			knight = false;
+			enemy = false;
+			obstacle = false;
+		}
+
+		public void clear() {
+			goal = false;
+			start = false;
+			knight = false;
+			enemy = false;
+			obstacle = false;
+		}
 	}
 }
