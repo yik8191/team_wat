ba901c684505e5c9b4168b0dcee92022dd7721a3
hakgagik <xddarkgalegh@aim.com>
2015-03-13 11:08:28 -0400

    Added project files of technical prototype

---
+++ deliverables/technical/desktop/src/edu/teamWat/rhythmKnights/gameplayPrototype/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.teamWat.rhythmKnights.gameplayPrototype.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.teamWat.rhythmKnights.gameplayPrototype.GDXRoot;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GDXRoot(), config);
+	}
+}
