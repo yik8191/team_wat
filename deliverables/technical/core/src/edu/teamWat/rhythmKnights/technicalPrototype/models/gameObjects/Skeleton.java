0a5ffebea2c3abc446ab1fa396ed66b7f4aa518e
Yu Austin Liu <ayl42@cornell.edu>
2015-03-15 15:31:32 -0400

    reworking

--- deliverables/technical/core/src/edu/teamWat/rhythmKnights/technicalPrototype/models/gameObjects/Skeleton.java
+++ deliverables/technical/core/src/edu/teamWat/rhythmKnights/technicalPrototype/models/gameObjects/Skeleton.java
@@ -1,4 +1,8 @@
 package edu.teamWat.rhythmKnights.technicalPrototype.models.gameObjects;
 
 public class Skeleton extends GameObject {
+
+	public Skeleton(int id, float x, float y) {
+		super(id, x, y);
+	}
 }
