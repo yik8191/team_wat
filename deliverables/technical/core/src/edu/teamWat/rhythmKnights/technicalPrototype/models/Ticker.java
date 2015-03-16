109bb8f6d2b32577c3d89486ebe65d0aea345c49
hakgagik <xddarkgalegh@aim.com>
2015-03-15 21:27:37 -0400

    Added ticker methods

--- deliverables/technical/core/src/edu/teamWat/rhythmKnights/technicalPrototype/models/Ticker.java
+++ deliverables/technical/core/src/edu/teamWat/rhythmKnights/technicalPrototype/models/Ticker.java
@@ -2,15 +2,38 @@ package edu.teamWat.rhythmKnights.technicalPrototype.models;
 
 import java.awt.*;
 
+
+/** Ticker */ // lol
 public class Ticker {
 
+	private TickerAction[] tickerActions;
+	private int beat;
 
-
-	public void draw(Canvas canvas) {
-
+	public Ticker(TickerAction[] actions) {
+		tickerActions = actions;
+		beat = 0;
 	}
 
-	public void getNextAction() {
-		
+	public void reset(TickerAction[] actions) {
+		beat = 0;
+	}
+
+	public void draw(Canvas canvas) {
+	}
+
+	public TickerAction getAction() {
+		return TickerAction.MOVE;
+	}
+
+	public void advance() {
+		beat++;
+		beat %= tickerActions.length;
+	}
+
+	public enum TickerAction {
+		MOVE,
+		DASH,
+		FREEZE,
+		FIREBALL
 	}
 }
