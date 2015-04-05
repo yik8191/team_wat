2d18f05ee049a1fd9eafb3e565b71cdc89aba1be
hakgagik <xddarkgalegh@aim.com>
2015-04-05 15:30:57 -0400

    Added level editor skeleton

--- deliverables/editor/core/src/editor/GDXRoot.java
+++ deliverables/editor/core/src/editor/GDXRoot.java
@@ -1,27 +1,104 @@
 package editor;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GDXRoot extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.assets.*;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.assets.loaders.*;
+import com.badlogic.gdx.assets.loaders.resolvers.*;
+
+import com.badlogic.gdx.graphics.g2d.freetype.*;
+import editor.utils.ScreenListener;
+import editor.views.GameCanvas;
+
+
+public class GDXRoot extends Game implements ScreenListener {
+	/** AssetManager to load game assets (textures, sounds, etc.) */
+	private AssetManager manager;
+	/** Drawing context to display graphics (VIEW CLASS) */
+	private GameCanvas canvas;
+	/** Player mode for the asset loading screen (CONTROLLER CLASS) */
+	private LoadingMode loading;
+	/** Player mode for the game proper (CONTROLLER CLASS) */
+	private GameMode playing;
+
+	public GDXRoot() {
+		// Start loading with the asset manager
+		manager = new AssetManager();
+
+		// Add font support to the asset manager
+		FileHandleResolver resolver = new InternalFileHandleResolver();
+		manager.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(resolver));
+		manager.setLoader(BitmapFont.class, ".ttf", new FreetypeFontLoader(resolver));
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	/**
+	 * Called when the Application is first created.  This is method immediately loads assets for the loading screen,
+	 * and prepares the asynchronous loader for all other assets.
+	 */
+	public void create() {
+		canvas = new GameCanvas();
+		loading = new LoadingMode(canvas, manager, 1);
+		playing = null;
+
+		loading.setScreenListener(this);
+		GameMode.PreLoadContent(manager); // Load game assets statically.
+		setScreen(loading);
+	}
+
+	/**
+	 * Called when the Application is destroyed.  This is preceded by a call to pause().
+	 */
+	public void dispose() {
+		// Call dispose on our children
+		Screen screen = getScreen();
+		setScreen(null);
+		screen.dispose();
+		canvas.dispose();
+		canvas = null;
+
+		// Unload all of the resources
+		GameMode.UnloadContent(manager);
+		manager.clear();
+		manager.dispose();
+		super.dispose();
+	}
+
+	/**
+	 * Called when the Application is resized.  This can happen at any point during a non-paused state but will never
+	 * happen before a call to create().
+	 *
+	 * @param width  The new width in pixels
+	 * @param height The new height in pixels
+	 */
+	public void resize(int width, int height) {
+		canvas.resize();
+		super.resize(width, height);
+	}
+
+	/**
+	 * The given screen has made a request to exit its player mode.  The value exitCode can be used to implement menu
+	 * options.
+	 *
+	 * @param screen   The screen requesting to exit
+	 * @param exitCode The state of the screen upon exit
+	 */
+	public void exitScreen(Screen screen, int exitCode) {
+		if (exitCode != 0) {
+			Gdx.app.error("GDXRoot", "Exit with error code " + exitCode, new RuntimeException());
+			Gdx.app.exit();
+		} else if (screen == loading) {
+			GameMode.LoadContent(manager);
+			playing = new GameMode(canvas);
+
+			playing.setScreenListener(this);
+			setScreen(playing);
+
+			loading.dispose();
+			loading = null;
+		} else {
+			// We quit the main application
+			Gdx.app.exit();
+		}
 	}
 }
--- deliverables/editor/desktop/src/editor/desktop/DesktopLauncher.java
+++ deliverables/editor/desktop/src/editor/desktop/DesktopLauncher.java
@@ -2,11 +2,18 @@ package editor.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import java.awt.*;
+
 import editor.GDXRoot;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		config.width = screenSize.width;
+		config.height = screenSize.height;
+		config.fullscreen = false;
 		new LwjglApplication(new GDXRoot(), config);
 	}
 }
