666817bc659d870bb199aee3304fb21a63130eea
cyt25 <cyt25@cornell.edu>
2015-04-05 21:16:43 -0400

    icon added

--- deliverables/alpha/desktop/src/edu/teamWat/rhythmKnights/alpha/desktop/DesktopLauncher.java
+++ deliverables/alpha/desktop/src/edu/teamWat/rhythmKnights/alpha/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package edu.teamWat.rhythmKnights.alpha.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -11,6 +12,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		config.addIcon("images/icon.png", Files.FileType.Internal);
 		config.width = screenSize.width;
 		config.height = screenSize.height;
 		config.fullscreen = false;
