68709330ebe2956987ad089f26edb4c9596f6d21
cyt25 <cyt25@cornell.edu>
2015-04-05 20:12:13 -0400

    fixing merge issues

--- deliverables/alpha/core/src/edu/teamWat/rhythmKnights/alpha/controllers/GameplayController.java
+++ deliverables/alpha/core/src/edu/teamWat/rhythmKnights/alpha/controllers/GameplayController.java
@@ -44,11 +44,11 @@ public class GameplayController {
 
 	public void initialize() {
 
-		board = JSONReader.parseFile("../../levels/level1.json");
+		board = JSONReader.parseFile("levels/level1.json");
         JSONReader.getObjects();
         ticker = JSONReader.initializeTicker();
 
-        collisionController = new CollisionController(board, gameObjects);
+        collisionController = new CollisionController(board, gameObjects, null);
 
 		knight = (Knight)gameObjects.getPlayer();
 		knight.setInvulnerable(true);
